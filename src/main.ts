0eae001d8a986f8c9fb72deacee7ef8918ee1d85
Chris Sprance <chrissprance@gmail.com>
2018-03-30 17:38:58 -0400

    :squirrel: Aggressive scurrying

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,25 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+export const squirrelList = [
+  'http://shipitsquirrel.github.io/images/ship%20it%20squirrel.png',
+  'http://images.cheezburger.com/completestore/2011/11/2/aa83c0c4-2123-4bd3-8097-966c9461b30c.jpg',
+  'http://images.cheezburger.com/completestore/2011/11/2/46e81db3-bead-4e2e-a157-8edd0339192f.jpg',
+  'http://28.media.tumblr.com/tumblr_lybw63nzPp1r5bvcto1_500.jpg',
+  'http://i.imgur.com/DPVM1.png',
+  'http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
+  'http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
+  'http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+  'http://img70.imageshack.us/img70/4853/cutesquirrels27rn9.jpg',
+  'http://img70.imageshack.us/img70/9615/cutesquirrels15ac7.jpg',
+  'https://dl.dropboxusercontent.com/u/602885/github/sniper-squirrel.jpg',
+  'http://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg',
+  'https://dl.dropboxusercontent.com/u/602885/github/soldier-squirrel.jpg',
+  'https://dl.dropboxusercontent.com/u/602885/github/squirrelmobster.jpeg'
+];
+export const single = (): string =>
+  squirrelList[Math.floor(Math.random() * squirrelList.length)];
+export const many = (amount: number = 5): string[] =>
+  squirrelList[Math.floor(Math.random() * squirrelList.length)];
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(name: string, delay: number = Delays.Medium): Promise<string> {
-  return new Promise(
-    (resolve: (value?: string) => void) => setTimeout(
-      () => resolve(`Hello, ${name}`),
-      delay,
-    ),
-  );
-}
-
-// Below are examples of using TSLint errors suppression
-// Here it is suppressing missing type definitions for greeter function
-
-export async function greeter(name) { // tslint:disable-line typedef
-  // tslint:disable-next-line no-unsafe-any no-return-await
-  return await delayedHello(name, Delays.Long);
-}
+export default {
+  many,
+  single
+};
